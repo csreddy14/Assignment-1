4229645cac22d4af1c8cf806207364c86ae665a5
csreddy14 <sura.reddy.19cse@bmu.edu.in>
2022-04-17 03:05:58 +0530

    first commit

---
+++ client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,32 @@
+import "./topbar.css";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+
+export default function Topbar() {
+  const { user } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">Web Chat</span>
+        </Link>
+      </div>
+      <div className="topbarRight">
+        <Link to={`/profile/${user.username}`}>
+          <img
+            src={
+              user.profilePicture
+                ? PF + user.profilePicture
+                : PF + "person/noAvatar.png"
+            }
+            alt=""
+            className="topbarImg"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+}
